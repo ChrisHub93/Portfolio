6754cb29440fbc71bafeb417f4a9176182c30b73
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-10-28 10:51:21 +0100

    Changed imprint to legal-notice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
-import { ImprintComponent } from './imprint/imprint.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 export const routes: Routes = [
-    { path: '', component: MainContentComponent },
-    { path: 'imprint', component: ImprintComponent },
+  { path: '', component: MainContentComponent },
+  { path: 'legal-notice', component: LegalNoticeComponent },
 ];
