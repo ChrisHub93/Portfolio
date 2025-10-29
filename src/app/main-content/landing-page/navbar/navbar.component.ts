67f8e8401f2661a08f0bc478115c466ce20d9a28
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-10-29 11:36:40 +0100

    Add navbar styles

--- src/app/main-content/landing-page/navbar/navbar.component.ts
+++ src/app/main-content/landing-page/navbar/navbar.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  languageEnglish: boolean = true;
 
 }
