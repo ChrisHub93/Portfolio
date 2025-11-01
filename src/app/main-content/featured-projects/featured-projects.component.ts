2050b326d0c1ca25ba2d705b6fc878c91b7f04fc
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-11-01 13:54:33 +0100

    show img with ts

--- src/app/main-content/featured-projects/featured-projects.component.ts
+++ src/app/main-content/featured-projects/featured-projects.component.ts
@@ -3,10 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-featured-projects',
   standalone: true,
-  imports: [],
   templateUrl: './featured-projects.component.html',
-  styleUrl: './featured-projects.component.scss'
+  styleUrl: './featured-projects.component.scss',
+  imports: [],
 })
-export class FeaturedProjectsComponent {
+export class FeaturedProjectsComponent  {
+  ngAfterViewInit(): void {
+    const projects = document.querySelectorAll<HTMLElement>('.project');
+    const previewImages =
+      document.querySelectorAll<HTMLImageElement>('.preview-img');
 
+    projects.forEach((project) => {
+      project.addEventListener('mouseenter', () => {
+        const imgId = project.dataset['img'];
+
+        previewImages.forEach((img) => {
+          img.style.display = 'none';
+        });
+
+        if (imgId) {
+          const targetImg = document.getElementById(
+            imgId
+          ) as HTMLImageElement | null;
+          if (targetImg) targetImg.style.display = 'block';
+        }
+      });
+
+      project.addEventListener('mouseleave', () => {
+        const imgId = project.dataset['img'];
+        if (imgId) {
+          const targetImg = document.getElementById(
+            imgId
+          ) as HTMLImageElement | null;
+          if (targetImg) targetImg.style.display = 'none';
+        }
+      });
+    });
+  }
 }
