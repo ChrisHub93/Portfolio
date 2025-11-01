5dd55eca31e2f26873d6bad7cbd84e3e5909f243
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-11-01 14:21:26 +0100

    show img with (mouseenter)

--- src/app/main-content/featured-projects/featured-projects.component.ts
+++ src/app/main-content/featured-projects/featured-projects.component.ts
@@ -8,36 +8,8 @@ import { Component } from '@angular/core';
   imports: [],
 })
 export class FeaturedProjectsComponent  {
-  ngAfterViewInit(): void {
-    const projects = document.querySelectorAll<HTMLElement>('.project');
-    const previewImages =
-      document.querySelectorAll<HTMLImageElement>('.preview-img');
 
-    projects.forEach((project) => {
-      project.addEventListener('mouseenter', () => {
-        const imgId = project.dataset['img'];
-
-        previewImages.forEach((img) => {
-          img.style.display = 'none';
-        });
-
-        if (imgId) {
-          const targetImg = document.getElementById(
-            imgId
-          ) as HTMLImageElement | null;
-          if (targetImg) targetImg.style.display = 'block';
-        }
-      });
-
-      project.addEventListener('mouseleave', () => {
-        const imgId = project.dataset['img'];
-        if (imgId) {
-          const targetImg = document.getElementById(
-            imgId
-          ) as HTMLImageElement | null;
-          if (targetImg) targetImg.style.display = 'none';
-        }
-      });
-    });
-  }
+  showJoinImg = false;
+  showPolloImg = false;
+  showBubbleImg = false;
 }
