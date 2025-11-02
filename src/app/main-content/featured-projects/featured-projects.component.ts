1e724d6c93eae0b483a3de6e8281f6fbf061d64a
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-11-02 10:53:42 +0100

    Refactor: Changed project rendering to dynamic list-based rendering

--- src/app/main-content/featured-projects/featured-projects.component.ts
+++ src/app/main-content/featured-projects/featured-projects.component.ts
@@ -8,9 +8,45 @@ import { Component } from '@angular/core';
   styleUrl: './featured-projects.component.scss',
   imports: [NgClass],
 })
-export class FeaturedProjectsComponent  {
-
+export class FeaturedProjectsComponent {
   showJoinImg = false;
-  showPolloImg = false;
-  showBubbleImg = false;
+  showElPolloLocoImg = false;
+  showDABubbleImg = false;
+
+  projects = [
+    {
+      name: 'Join',
+      skills: ['TypeScript', 'HTML', 'CSS', 'Firebase'],
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+    },
+    {
+      name: 'El Pollo Loco',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      description:
+        'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen. ',
+    },
+    {
+      name: 'DA Bubble',
+      skills: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+      description:
+        'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen. ',
+    },
+  ];
+
+  showImage(name: string) {
+    this.resetImages();
+    const key = `show${name.replace(/\s+/g, '')}Img`;
+    (this as any)[key] = true;
+  }
+
+  hideImage(name: string) {
+    (this as any)[`show${name}Img`] = false;
+  }
+
+  private resetImages() {
+    this.showJoinImg = false;
+    this.showElPolloLocoImg = false;
+    this.showDABubbleImg = false;
+  }
 }
