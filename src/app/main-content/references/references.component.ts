33d9913702ce744a35c561de0d2c364a89e289c6
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-11-04 17:49:59 +0100

    Add references data

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -5,8 +5,33 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './references.component.html',
-  styleUrl: './references.component.scss'
+  styleUrl: './references.component.scss',
 })
 export class ReferencesComponent {
-
+  references = [
+    {
+      active: false,
+      name: 'T.Schulz - Frontend Developer',
+      comment:
+        'Our project benefited enormously from Lukas efficient way of working.',
+    },
+    {
+      active: true,
+      name: 'H.Janisch - Team Partner',
+      comment:
+        'Lukas has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the success of our project. ',
+    },
+    {
+      active: false,
+      name: 'A. Fischer - Team Partner',
+      comment:
+        'I had the good fortune of working with Simon in a group project at the Developer Akademie that involved a lot of effort. He always stayed calm, cool, and focused, and made sure our team was set up for success. He is super knowledgeable, easy to work with, and I did happily work with him again given the chance.',
+    },
+    {
+      active: false,
+      name: 'T.Schulz - Frontend Developer',
+      comment:
+        'Our project benefited enormously from Simon efficient way of working.',
+    },
+  ];
 }
