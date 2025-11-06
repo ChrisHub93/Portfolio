22ab96371542639af448b20105d69a8850f58135
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-11-06 11:15:17 +0100

    update scroll logic

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,4 +1,9 @@
-import { Component, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  ViewChildren,
+  QueryList,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-references',
@@ -7,33 +12,33 @@ import { Component, ViewChildren, QueryList, AfterViewInit } from '@angular/core
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss',
 })
-export class ReferencesComponent implements AfterViewInit{
-  @ViewChildren('commentItem') commentItems!: QueryList<HTMLElement>;
-  currentID:number = 2;
+export class ReferencesComponent {
+
+  currentID: number = 1;
   references = [
     {
-      id: "comment_1",
+      id: 'comment_1',
       active: false,
       autor: 'T.Schulz - Frontend Developer',
       comment:
         'Our project benefited enormously from Lukas efficient way of working.',
     },
     {
-      id: "comment_2",
+      id: 'comment_2',
       active: true,
       autor: 'H.Janisch - Team Partner',
       comment:
         'Lukas has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the success of our project. ',
     },
     {
-      id: "comment_3",
+      id: 'comment_3',
       active: false,
       autor: 'A. Fischer - Team Partner',
       comment:
         'I had the good fortune of working with Simon in a group project at the Developer Akademie that involved a lot of effort. He always stayed calm, cool, and focused, and made sure our team was set up for success. He is super knowledgeable, easy to work with, and I did happily work with him again given the chance.',
     },
     {
-      id: "comment_4",
+      id: 'comment_4',
       active: false,
       autor: 'T.Schulz - Frontend Developer',
       comment:
@@ -41,18 +46,27 @@ export class ReferencesComponent implements AfterViewInit{
     },
   ];
 
-  ngAfterViewInit() {
-    this.scrollActiveIntoView();
-  }
-  scrollActiveIntoView() {
-    const activeIndex = this.references.findIndex(r => r.active);
-    const activeElement = this.commentItems.toArray()[activeIndex];
-
-    activeElement?.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
-  }
-
   setActive(id: string) {
-    this.references.forEach(ref => ref.active = ref.id === id);
-    this.scrollActiveIntoView();
+    this.references.forEach((ref) => (ref.active = ref.id === id));
+  }
+
+  next() {
+    if (this.currentID < this.references.length - 1) {
+      this.currentID++;
+    }
+    this.updateActive();
+  }
+
+  prev() {
+    if (this.currentID > 0) {
+      this.currentID--;
+    }
+    this.updateActive();
+  }
+
+  updateActive() {
+    this.references.forEach(
+      (r, index) => (r.active = index === this.currentID)
+    );
   }
 }
