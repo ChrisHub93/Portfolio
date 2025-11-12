61c7fc82b289873f72676ac856458f4e24b62e29
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-11-12 16:43:02 +0100

    Refactored hover logic in the featured-project section to improve interactivity and maintainability.

--- src/app/main-content/featured-projects/featured-projects.component.ts
+++ src/app/main-content/featured-projects/featured-projects.component.ts
@@ -1,45 +1,41 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { ProjectInfoComponent } from "./project-info/project-info.component";
+import { ProjectInfoComponent } from './project-info/project-info.component';
 
 @Component({
   selector: 'app-featured-projects',
   standalone: true,
   templateUrl: './featured-projects.component.html',
   styleUrl: './featured-projects.component.scss',
-  imports: [NgClass, ProjectInfoComponent],
+  imports: [ProjectInfoComponent],
 })
 export class FeaturedProjectsComponent {
-  showJoinImg = false;
-  showElPolloLocoImg = false;
-  showPokedexImg = false;
-  showDABubbleImg = false;
   projectInfoOpen = false;
-  selectedProject: any = null; 
+  selectedProject: any = null;
   projects = [
     {
-      id :'01',
+      id: '01',
       name: 'Join',
       skills: ['TypeScript', 'HTML', 'CSS', 'Firebase'],
       description:
         'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
     },
     {
-      id :'02',
+      id: '02',
       name: 'El Pollo Loco',
       skills: ['HTML', 'CSS', 'JavaScript'],
       description:
         'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen. ',
     },
     {
-      id :'03',
+      id: '03',
       name: 'Pokedex',
-      skills: [ 'JavaScript', 'HTML', 'CSS', 'REST-API'],
+      skills: ['JavaScript', 'HTML', 'CSS', 'REST-API'],
       description:
         'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.',
     },
     {
-      id :'04',
+      id: '04',
       name: 'DA Bubble',
       skills: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
       description:
@@ -53,22 +49,5 @@ export class FeaturedProjectsComponent {
 
   closeProjectInfo() {
     this.selectedProject = null;
-  }
-
-  showImage(name: string) {
-    this.resetImages();
-    const key = `show${name.replace(/\s+/g, '')}Img`;
-    (this as any)[key] = true;
-  }
-
-  hideImage() {
-    this.resetImages();
-  }
-
-  private resetImages() {
-    this.showJoinImg = false;
-    this.showElPolloLocoImg = false;
-    this.showDABubbleImg = false;
-    this.showPokedexImg = false;
   }
 }
