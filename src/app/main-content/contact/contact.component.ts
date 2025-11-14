dc79f833ec78759fe38b8201ae2bdabfca855825
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-11-14 16:01:19 +0100

    Added a no-scroll function that activates when the user receives the success message.

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -8,51 +8,60 @@ import { SuccessMessageComponent } from './success-message/success-message.compo
   standalone: true,
   imports: [FormsModule, SuccessMessageComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  http = inject(HttpClient);
 
-http = inject(HttpClient);
+  accepted = false;
+  showMessage = false;
 
-accepted =false;
-showMessage= false;
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  };
 
-contactData ={
-  name: "",
-  email: "",
-  message: "",
-};
+  mailTest = true;
 
-mailTest = true;
-
-post = {
-  endPoint: 'https://deineDomain.de/sendMail.php',
-  body: (payload: any) => JSON.stringify(payload),
-  options: {
-    headers: {
-      'Content-Type': 'text/plain',
-      responseType: 'text',
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
     },
-  },
-};
+  };
 
-onSubmit(ngForm: NgForm) {
-  if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-    this.http.post(this.post.endPoint, this.post.body(this.contactData))
-      .subscribe({
-        next: (response) => {
-
-          ngForm.resetForm();
-        },
-        error: (error) => {
-          console.error(error);
-        },
-        complete: () => console.info('send post complete'),
-      });
-  } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-    console.log(this.contactData);
-    ngForm.resetForm();
-
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      console.log(this.contactData);
+      ngForm.resetForm();
+      this.showSuccesMessage();
+    }
   }
-}
+
+  showSuccesMessage() {
+    this.showMessage = true;
+    document.body.classList.add('no-scroll');
+
+    setInterval(() => {
+      this.showMessage = false;
+      document.body.classList.remove('no-scroll');
+    }, 2900);
+  }
 }
