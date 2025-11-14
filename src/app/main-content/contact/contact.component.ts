8651f2399603310d5c938dd39d62c9a9b992f007
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-11-14 15:05:53 +0100

    Adde succes-message components for the "contact" section

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { SuccessMessageComponent } from './success-message/success-message.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, SuccessMessageComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -14,6 +15,7 @@ export class ContactComponent {
 http = inject(HttpClient);
 
 accepted =false;
+showMessage= false;
 
 contactData ={
   name: "",
