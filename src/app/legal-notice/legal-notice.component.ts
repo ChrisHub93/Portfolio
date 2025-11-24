1affe1d6c3b5cdb10c8aa7563d0bb383dcee36fc
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-11-24 14:32:09 +0100

    Added responsive styling to the legal-notice section for improved layout across devices.

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import { LanguageService } from '../language.service';
+import { NavbarMobileComponent } from "../shared/components/navbar-mobile/navbar-mobile.component";
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, NavbarMobileComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss',
 })
